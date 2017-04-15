d382127b4766fb91c94d3b0d2037e3f4286641e6
varun <sriram.varun672@gmail.com>
2017-04-15 13:12:13 -0500

    Titanic Survival

--- titanic.py
+++ titanic.py
@@ -5,3 +5,42 @@ Created on Sat Apr 15 12:12:19 2017
 @author: Varun
 """
 
+import pandas as pd
+import numpy as np
+
+train_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
+train = pd.read_csv(train_url)
+
+test_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
+test = pd.read_csv(test_url)
+
+train.head()
+
+train.describe()
+
+data = [train, test]
+
+data.head()
+
+train.isnull().sum()
+
+test.isnull().sum()
+
+train.columns.values
+
+
+print(train.info())
+
+train.describe(include=['O'])
+
+print(train[['Pclass','Survived']].groupby(['Pclass']).mean())
+
+print(pd.crosstab(train['Pclass'], train['Survived']))
+
+print(train[['Sex','Survived']].groupby(['Sex']).mean())
+
+print(train[['SibSp','Survived']].groupby(['SibSp']).mean())
+
+
+print(train[['Parch','Survived']].groupby(['Parch']).mean())
+
